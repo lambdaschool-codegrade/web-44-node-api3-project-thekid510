208424433383bd74921f3bb774ae8c838dfe7716
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-08-04 16:45:39 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const { listen } = require('./api/server.js');
+const server = require('./api/server.js');
+
+
+server.listen(5000, () => {
+    console.log('\n*Server running on http://localhost:5000 *\n')
+});
